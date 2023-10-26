53ebd900027346548e82141a4c417737b9f24660
sunuwars77 <62463878+sunuwars77@users.noreply.github.com>
2023-10-26 16:48:26 +0000

    variables and constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12224
+let accountEmail = "sonam@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+ let accountState;
+//accountId = 2
+accountEmail="hh@gmail.com"
+accountPassword="123456"
+accountCity = "nepal"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
